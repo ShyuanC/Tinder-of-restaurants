c6c49dd889b7cbb01ad6db3140990ac5a645619f
YinhongCao <144277168+YinhongCao@users.noreply.github.com>
2025-02-23 23:43:57 -0500

    input_filter_Module.py Feb 23, 2025 (finally working ver.)

--- UI/Opening_Page.py
+++ UI/Opening_Page.py
@@ -3,16 +3,20 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.core.window import Window
+import os
 
 class OpeningPage(App):
     def build(self):
         Window.size = (360 ,640)
         Window.clearcolor = (1, 0.71, 0.1, 0.9)
 
+        def asset_path(filename):
+            return os.path.join(os.path.dirname(__file__), '..', 'assets', filename)
+
         # Create a main layout with vertical orientation
         main_layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
 
-        logo = Image(source='assets/logo.png', size_hint=(1, 1), allow_stretch=True)
+        logo = Image(source=asset_path('logo.png'), size_hint=(1, 1), allow_stretch=True)
         main_layout.add_widget(logo)
 
         Clock.schedule_once(self.switch_to_main_app, 3)
